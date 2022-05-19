3c34a98b2371ae1c0b2eaf055195506b7ac8a70e
Djmia <100264398+Djmia@users.noreply.github.com>
2022-05-19 10:24:23 +0300

    Add files via upload

---
+++ Rubik.java
@@ -0,0 +1,17 @@
+
+
+  public class Rubik {
+      public static void main(String[] args) {
+          String name = "Vanko";
+          String cube = "I am Speed Cuber";
+          System.out.printf("My name is %s, %n%s" 
+          ,name, cube);
+      }
+  }
+  
+  
+  
+  
+  
+  
+  
